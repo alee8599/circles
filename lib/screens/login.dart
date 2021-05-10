7c712bc30be175a560e9ef9bb42f55fcb4711a8e
nnaik39 <nanni.naik@gmail.com>
2021-05-09 20:06:49 -0700

    added error handling to login/logout

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,6 +1,7 @@
 import 'package:circles/screens/home_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -50,23 +51,42 @@ class _LoginScreenState extends State<LoginScreen> {
                     child: Text('Sign in'),
                     style: ElevatedButton.styleFrom(
                         primary: Theme.of(context).accentColor),
-                    onPressed: () {
-                      auth.signInWithEmailAndPassword(
-                          email: _email, password: _password);
-                      Navigator.of(context).pushReplacement(
-                          MaterialPageRoute(builder: (context) => Home()));
-                    }),
+                    onPressed: () => _signin(_email, _password)),
                 ElevatedButton(
                     child: Text('Sign up'),
                     style: ElevatedButton.styleFrom(
                         primary: Theme.of(context).accentColor),
-                    onPressed: () {
-                      auth.createUserWithEmailAndPassword(
-                          email: _email, password: _password);
-                    })
+                    onPressed: () => _signup(_email, _password))
               ],
             )
           ],
         ));
+  }
+
+  _signin(String _email, String _password) async {
+    try {
+      await auth.signInWithEmailAndPassword(email: _email, password: _password);
+
+      // Success
+      Navigator.of(context)
+          .pushReplacement(MaterialPageRoute(builder: (context) => Home()));
+    } on FirebaseAuthException catch (error) {
+      print(error.message);
+      Fluttertoast.showToast(msg: error.message, gravity: ToastGravity.TOP);
+    }
+  }
+
+  _signup(String _email, String _password) async {
+    try {
+      await auth.createUserWithEmailAndPassword(
+          email: _email, password: _password);
+
+      // Success
+      Navigator.of(context)
+          .pushReplacement(MaterialPageRoute(builder: (context) => Home()));
+    } on FirebaseAuthException catch (error) {
+      print(error.message);
+      Fluttertoast.showToast(msg: error.message, gravity: ToastGravity.TOP);
+    }
   }
 }
