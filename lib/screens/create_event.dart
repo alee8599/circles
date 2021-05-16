052f180ac12e1baa985062dab7ca9a95fc00eb4c
Ryan Peng <ryanpeng@DN0a1fa911.SUNet>
2021-05-16 11:46:38 -0700

    added more space between fields, textfield background color, and invite friends button

--- lib/screens/create_event.dart
+++ lib/screens/create_event.dart
@@ -14,68 +14,88 @@ class _CreateEventState extends State<CreateEvent> {
             title: Text(
           "Create Your Event",
           textAlign: TextAlign.left,
-          style: TextStyle(color: Colors.white, fontSize: 30.0),
+          style: TextStyle(color: Colors.white, fontSize: 30.0, fontFamily: "Kayak Sans"),
         )),
         body: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Padding(
-                padding: EdgeInsets.only(top: 20.0, bottom: 16.0, left: 8.0),
+                padding: EdgeInsets.only(top: 20.0, bottom: 16.0, left: 150.0),
                 child: Text(
                   "Event Details",
                   style: TextStyle(
-                    fontSize: 24.0, // insert your font size here
+                    fontSize: 24.0, 
                   ),
                 )),
             Padding(
-                padding: EdgeInsets.only(top: 12.0, left: 8.0),
+                padding: EdgeInsets.only(top: 12.0, left: 18.0),
                 child: Text("Name", style: TextStyle(fontSize: 20.0))),
             Padding(
-              padding: const EdgeInsets.only(left: 8.0),
+              padding: const EdgeInsets.only(left: 8.0, bottom: 20.0),
               child: TextField(
-                keyboardType: TextInputType.emailAddress,
-                //decoration: InputDecoration(hintText: 'Email'),
-                // onChanged: (value) {
-                //   setState(() {
-                //     _email = value.trim();
-                //   });
-                // },
+                decoration: InputDecoration( fillColor: WhiteGrey, filled: true)
               ),
             ),
             Padding(
-                padding: EdgeInsets.only(top: 8.0, left: 8.0),
+                padding: EdgeInsets.only(top: 8.0, left: 20.0),
                 child: Text("Date", style: TextStyle(fontSize: 20.0))),
             Padding(
-              padding: const EdgeInsets.only(left: 8.0),
+              padding: const EdgeInsets.only(left: 8.0, bottom: 20.0),
               child: TextField(
-                obscureText: true,
+                decoration: InputDecoration( fillColor: WhiteGrey, filled: true)
               ),
             ),
             Padding(
-                padding: EdgeInsets.only(top: 8.0, left: 8.0),
+                padding: EdgeInsets.only(top: 8.0, left: 20.0),
                 child: Text("Time", style: TextStyle(fontSize: 20.0))),
             Padding(
-              padding: const EdgeInsets.only(left: 8.0),
+              padding: const EdgeInsets.only(left: 8.0, bottom: 20.0),
               child: TextField(
-                obscureText: true,
+                decoration: InputDecoration( fillColor: WhiteGrey, filled: true)
               ),
             ),
             Padding(
-                padding: EdgeInsets.only(top: 8.0, left: 8.0),
+                padding: EdgeInsets.only(top: 8.0, left: 20.0),
                 child: Text("Location", style: TextStyle(fontSize: 20.0))),
             Padding(
-              padding: const EdgeInsets.only(left: 8.0),
+              padding: const EdgeInsets.only(left: 8.0, bottom: 20.0),
               child: TextField(
-                obscureText: true,
+                decoration: InputDecoration( fillColor: WhiteGrey, filled: true)
               ),
             ),
             Padding(
-                padding: EdgeInsets.only(top: 8.0, left: 8.0),
+                padding: EdgeInsets.only(top: 8.0, left: 20.0),
                 child: Text("Description", style: TextStyle(fontSize: 20.0))),
             Padding(
               padding: const EdgeInsets.only(left: 8.0, bottom: 40.0),
-              child: TextField(obscureText: true),
+              child: TextField(
+                decoration: InputDecoration( fillColor: WhiteGrey, filled: true)
+              ),
             ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                ElevatedButton(
+                  child: Text(
+                    '+ Invite Friends',
+                    style: TextStyle(fontSize: 30.0),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    primary: RedOrange,
+                  ),
+                  onPressed: () {
+                    print('Pressed');
+                  },
+                ),
+                // ElevatedButton(
+                //     child: Text('Sign up'),
+                //     style: ElevatedButton.styleFrom(
+                //         primary: Theme.of(context).accentColor),
+                //     //onPressed: () => _signup(_email, _password))
+                // )
+              ],
+            ),
+            Padding(padding: EdgeInsets.only(bottom: 20.0)),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
