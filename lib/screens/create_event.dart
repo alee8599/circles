36574a75473000fe075f75c2016daec1b526d060
Ryan Peng <ryanpeng@DN0a1fa911.SUNet>
2021-05-16 15:51:35 -0700

    updated

--- lib/screens/create_event.dart
+++ lib/screens/create_event.dart
@@ -10,116 +10,124 @@ class _CreateEventState extends State<CreateEvent> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            title: Text(
-          "Create Your Event",
-          textAlign: TextAlign.left,
-          style: TextStyle(color: Colors.white, fontSize: 30.0, fontFamily: "Kayak Sans"),
-        )),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-                padding: EdgeInsets.only(top: 20.0, bottom: 16.0, left: 150.0),
+      appBar: AppBar(
+        title: Text(
+        "Create Your Event",
+        textAlign: TextAlign.left,
+        style: TextStyle(
+            color: Colors.white, fontSize: 30.0, fontFamily: "Kayak Sans"),
+        ),
+        automaticallyImplyLeading: true,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          color: WhiteGrey,
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+              padding: EdgeInsets.only(top: 20.0, bottom: 16.0, left: 150.0),
+              child: Text(
+                "Event Details",
+                style: TextStyle(
+                  fontSize: 24.0,
+                ),
+              )),
+          Padding(
+              padding: EdgeInsets.only(top: 12.0, left: 18.0),
+              child: Text("Name", style: TextStyle(fontSize: 20.0))),
+          Padding(
+            padding: const EdgeInsets.only(left: 8.0, bottom: 20.0),
+            child: TextField(
+                decoration:
+                    InputDecoration(fillColor: WhiteGrey, filled: true)),
+          ),
+          Padding(
+              padding: EdgeInsets.only(top: 8.0, left: 20.0),
+              child: Text("Date", style: TextStyle(fontSize: 20.0))),
+          Padding(
+            padding: const EdgeInsets.only(left: 8.0, bottom: 20.0),
+            child: TextField(
+                decoration:
+                    InputDecoration(fillColor: WhiteGrey, filled: true)),
+          ),
+          Padding(
+              padding: EdgeInsets.only(top: 8.0, left: 20.0),
+              child: Text("Time", style: TextStyle(fontSize: 20.0))),
+          Padding(
+            padding: const EdgeInsets.only(left: 8.0, bottom: 20.0),
+            child: TextField(
+                decoration:
+                    InputDecoration(fillColor: WhiteGrey, filled: true)),
+          ),
+          Padding(
+              padding: EdgeInsets.only(top: 8.0, left: 20.0),
+              child: Text("Location", style: TextStyle(fontSize: 20.0))),
+          Padding(
+            padding: const EdgeInsets.only(left: 8.0, bottom: 20.0),
+            child: TextField(
+                decoration:
+                    InputDecoration(fillColor: WhiteGrey, filled: true)),
+          ),
+          Padding(
+              padding: EdgeInsets.only(top: 8.0, left: 20.0),
+              child: Text("Description", style: TextStyle(fontSize: 20.0))),
+          Padding(
+            padding: const EdgeInsets.only(left: 8.0, bottom: 40.0),
+            child: TextField(
+                decoration:
+                    InputDecoration(fillColor: WhiteGrey, filled: true)),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              ElevatedButton(
                 child: Text(
-                  "Event Details",
-                  style: TextStyle(
-                    fontSize: 24.0, 
-                  ),
-                )),
-            Padding(
-                padding: EdgeInsets.only(top: 12.0, left: 18.0),
-                child: Text("Name", style: TextStyle(fontSize: 20.0))),
-            Padding(
-              padding: const EdgeInsets.only(left: 8.0, bottom: 20.0),
-              child: TextField(
-                decoration: InputDecoration( fillColor: WhiteGrey, filled: true)
-              ),
-            ),
-            Padding(
-                padding: EdgeInsets.only(top: 8.0, left: 20.0),
-                child: Text("Date", style: TextStyle(fontSize: 20.0))),
-            Padding(
-              padding: const EdgeInsets.only(left: 8.0, bottom: 20.0),
-              child: TextField(
-                decoration: InputDecoration( fillColor: WhiteGrey, filled: true)
-              ),
-            ),
-            Padding(
-                padding: EdgeInsets.only(top: 8.0, left: 20.0),
-                child: Text("Time", style: TextStyle(fontSize: 20.0))),
-            Padding(
-              padding: const EdgeInsets.only(left: 8.0, bottom: 20.0),
-              child: TextField(
-                decoration: InputDecoration( fillColor: WhiteGrey, filled: true)
-              ),
-            ),
-            Padding(
-                padding: EdgeInsets.only(top: 8.0, left: 20.0),
-                child: Text("Location", style: TextStyle(fontSize: 20.0))),
-            Padding(
-              padding: const EdgeInsets.only(left: 8.0, bottom: 20.0),
-              child: TextField(
-                decoration: InputDecoration( fillColor: WhiteGrey, filled: true)
-              ),
-            ),
-            Padding(
-                padding: EdgeInsets.only(top: 8.0, left: 20.0),
-                child: Text("Description", style: TextStyle(fontSize: 20.0))),
-            Padding(
-              padding: const EdgeInsets.only(left: 8.0, bottom: 40.0),
-              child: TextField(
-                decoration: InputDecoration( fillColor: WhiteGrey, filled: true)
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                ElevatedButton(
-                  child: Text(
-                    '+ Invite Friends',
-                    style: TextStyle(fontSize: 30.0),
-                  ),
-                  style: ElevatedButton.styleFrom(
-                    primary: RedOrange,
-                  ),
-                  onPressed: () {
-                    print('Pressed');
-                  },
+                  '+ Invite Friends',
+                  style: TextStyle(fontSize: 30.0),
                 ),
-                // ElevatedButton(
-                //     child: Text('Sign up'),
-                //     style: ElevatedButton.styleFrom(
-                //         primary: Theme.of(context).accentColor),
-                //     //onPressed: () => _signup(_email, _password))
-                // )
-              ],
-            ),
-            Padding(padding: EdgeInsets.only(bottom: 20.0)),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                ElevatedButton(
-                  child: Text(
-                    'Create Event',
-                    style: TextStyle(fontSize: 30.0),
-                  ),
-                  style: ElevatedButton.styleFrom(
-                    primary: RedOrange,
-                  ),
-                  onPressed: () {
-                    print('Pressed');
-                  },
+                style: ElevatedButton.styleFrom(
+                  primary: RedOrange,
                 ),
-                // ElevatedButton(
-                //     child: Text('Sign up'),
-                //     style: ElevatedButton.styleFrom(
-                //         primary: Theme.of(context).accentColor),
-                //     //onPressed: () => _signup(_email, _password))
-                // )
-              ],
-            )
-          ],
-        ));
+                onPressed: () {
+                  print('Pressed');
+                },
+              ),
+              // ElevatedButton(
+              //     child: Text('Sign up'),
+              //     style: ElevatedButton.styleFrom(
+              //         primary: Theme.of(context).accentColor),
+              //     //onPressed: () => _signup(_email, _password))
+              // )
+            ],
+          ),
+          Padding(padding: EdgeInsets.only(bottom: 20.0)),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              ElevatedButton(
+                child: Text(
+                  'Create Event',
+                  style: TextStyle(fontSize: 30.0),
+                ),
+                style: ElevatedButton.styleFrom(
+                  primary: RedOrange,
+                ),
+                onPressed: () {
+                  print('Pressed');
+                },
+              ),
+              // ElevatedButton(
+              //     child: Text('Sign up'),
+              //     style: ElevatedButton.styleFrom(
+              //         primary: Theme.of(context).accentColor),
+              //     //onPressed: () => _signup(_email, _password))
+              // )
+            ],
+          )
+        ],
+      ));
   }
 }
